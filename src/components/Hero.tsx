562b7304b2da128c41b1bc2a7c3c40245b8e9cf3
Abukstech <abdulmajeedbamidele4@gmail.com>
2026-03-05 02:36:44 -0800

    chore:re update the content for the hero section again

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export const Hero = () => {
     <div className="max-w-md">
       <h1 className="text-5xl font-bold">Explore Varieties of Cocktail Brands</h1>
       <p className="py-6">
-        Discover a world of flavors with our extensive collection of cocktail recipes. Whether you're a seasoned mixologist or just starting your cocktail journey, we have something for everyone. From classic concoctions to innovative blends, our curated selection of cocktail brands will tantalize your taste buds and inspire your creativity. Cheers to unforgettable moments and delicious sips!
+        Discover a world of flavors with our extensive collection of cocktail recipes. Whether you're a seasoned mixologist or just starting your cocktail journey, we have something for everyone. 
       </p>
      
     </div>
